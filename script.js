7ac6ed8511440a84a37df2032d34a677230b53a4
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-01-22 23:53:50 +0600

    added smooth scrolling effect

--- script.js
+++ script.js
@@ -32,3 +32,19 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+//----------------------
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+
+  //old school
+  window.scrollTo({
+    left: s1coords.left + window.pageYOffset,
+    top: s1coords.top + window.pageYOffset,
+    behavior: 'smooth',
+  });
+  //modern
+  // section1.scrollIntoView({ behavior: 'smooth' });
+});
