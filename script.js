77364252c45f8a53cae393ccea36bb327ec20690
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-01-23 20:11:39 +0600

    efficient smooth navigaion with event delegation

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 'use strict';
-
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,19 +33,81 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
-//----------------------
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+
+//page navigaion
 
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
 
   //old school
   window.scrollTo({
-    left: s1coords.left + window.pageYOffset,
+    left: s1coords.left + window.pageXOffset,
     top: s1coords.top + window.pageYOffset,
     behavior: 'smooth',
   });
   //modern
   // section1.scrollIntoView({ behavior: 'smooth' });
 });
+//this is not efficient
+// document.querySelectorAll('.nav__link').forEach(el =>
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     // console.log(document.querySelector(e.target.getAttribute('href')));
+//     // console.log(this);
+//     const id = this.getAttribute('href');
+//     // console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//     // console.log(e.target);
+//   })
+// );
+
+//this is  efficient || EVENT DELEGATION
+
+//1. add event listener to common parent
+//2. determine element originated the event
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // console.log(e.target);
+  e.preventDefault();
+  //matching tech
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    // console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+// //event
+// const alerth1 = function () {
+//   alert('enter');
+//   h1.removeEventListener('mouseenter', alerth1);
+// };
+// const h1 = document.querySelector('h1');
+// h1.addEventListener('mouseenter', alerth1);
+
+// //bubbling
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+// console.log(randomColor());
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('link', e.target, e.currentTarget);
+//   // e.stopPropagation();
+// });
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('links', e.target, e.currentTarget);
+//   // e.stopPropagation();
+// });
+
+// document.querySelector('.nav').addEventListener(
+//   'click',
+//   function (e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log('nav', e.target, e.currentTarget);
+//     // e.stopPropagation();
+//   },
+//   false
+// );
